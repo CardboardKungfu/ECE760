0e06d44ed2ee0199f250299e7ac175b07e7737ff
Jed Pulley <jedediah.pulley@gmail.com>
2024-02-23 11:49:33 -0600

    Final Grad Ascent Algo

--- HW3/grad_ascent.py
+++ HW3/grad_ascent.py
@@ -1,17 +1,25 @@
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-def gradient_ascent(X, y, learning_rate, iterations):
-    theta_hat = np.linalg.inv(X.T @ X) @ X.T @ y
+def likelihood(y_star, y_hat):
+    epsilon = 1e-7 # Define epsilon because log(0) is not defined
+    y_hat = np.maximum(np.full(y_hat.shape, epsilon), np.minimum(np.full(y_hat.shape, 1 - epsilon), y_hat))
+    log_likelihood = (y_star * np.log(y_hat) + (1 - y_star) * np.log(1 - y_hat))
+    return np.mean(log_likelihood)
+
+def gradient_ascent(X, y_true, learning_rate, iterations):
+    # use linear regression for best guess of theta_hat
+    theta_hat = np.linalg.inv(X.T @ X) @ X.T @ y_true
     
     for _ in range(iterations):
         gradient = 0
     
         for i, row in enumerate(X):
             x_i = row.T
-            gradient += (y[i] - sigmoid(np.dot(theta_hat, x_i))) * x_i
+            gradient += (y_true[i] - sigmoid(np.dot(theta_hat, x_i))) * x_i
         
         theta_hat += learning_rate * gradient
     
@@ -20,16 +28,14 @@ def gradient_ascent(X, y, learning_rate, iterations):
 data = np.loadtxt("titanic_data.csv", delimiter=',', skiprows=1)
 
 y = data[:, 0] # response vector (survived or not)
+X = data[:, 1:-1]
 
-fare_col_normed = (data[:,-1] / np.max(data[:,-1])).reshape(-1, 1) # normalize the fare column, ideally to avoid exp overflow
-X = np.hstack((data[:, 1:-1], fare_col_normed))
+# fare_col_normed = (data[:,-1] / np.max(data[:,-1])).reshape(-1, 1) # normalize the fare column, ideally to avoid exp overflow
+# X = np.hstack((data[:, 1:-1], fare_col_normed))
 
-# print("X")
-# print(X)
 learning_rate = 0.001
 iterations = 1000
 
-# Call gradient ascent function
 coefficients = gradient_ascent(X, y, learning_rate, iterations)
 
 print("Coefficients:", coefficients)
